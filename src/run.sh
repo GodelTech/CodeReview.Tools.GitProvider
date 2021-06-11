4da83d21c177e717371aae96ab196f7ee3ccfff0
Arseniy Golovnev <Golovnev@BlackShadow.Name>
2021-06-11 12:47:31 +0300

    GIT_USE_BASIC renamed to GIT_USE_BASIC_AUTH_SCHEME

--- src/run.sh
+++ src/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -n "$GIT_USE_BASIC" ]
+if [ -n "$GIT_USE_BASIC_AUTH_SCHEME" ]
 then
 unset GIT_ASKPASS
 git -c http.extraHeader='Authorization: Basic '"$GIT_ACCESS_TOKEN"'' clone $GIT_REPOSITORY_URL /src && git switch $GIT_BRANCH
