c92acbbb703a87526d8461547e6deca9c39fadb2
Arseniy Golovnev <Golovnev@BlackShadow.Name>
2021-06-11 14:19:46 +0300

    refactoring

--- src/run.sh
+++ src/run.sh
@@ -5,5 +5,6 @@ then
 unset GIT_ASKPASS
 git -c http.extraHeader='Authorization: Basic '"$GIT_ACCESS_TOKEN"'' clone $GIT_REPOSITORY_URL /src && git switch $GIT_BRANCH
 else
+echo 'echo $GIT_ACCESS_TOKEN' > /root/git-askpass && chmod 777 /root/git-askpass
 git clone $GIT_REPOSITORY_URL /src && git switch $GIT_BRANCH
 fi
